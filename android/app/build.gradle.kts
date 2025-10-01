85d78e9f6913ec1eed91d4ba01624c08fd81f1aa
JamieMcClymont <158759587+JamieMcClymont@users.noreply.github.com>
2025-10-01 13:38:01 +0100

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,77 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+plugins {
+    id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+def keystoreProperties = new Properties()
+def keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(new FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "com.jamie.buckit"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.jamie.buckit"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            val store = keystoreProperties["storeFile"] as String?
+            if (store != null) {
+                storeFile = file(store)
+            }
+            keyAlias = (keystoreProperties["keyAlias"] ?: "") as String
+            keyPassword = (keystoreProperties["keyPassword"] ?: "") as String
+            storePassword = (keystoreProperties["storePassword"] ?: "") as String
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            // REMOVE this line if present: signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")  // <-- use your release key
+            // Optional optimizations:
+            // isMinifyEnabled = true
+            // isShrinkResources = true
+            // proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/jamie/buckit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jamie.buckit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
